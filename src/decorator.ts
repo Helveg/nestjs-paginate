aafc284b6175b08ed20aa1c0314c9e7b2f6a5613
Robin De Schepper <robin.deschepper93@gmail.com>
2026-04-20 15:28:10 +0200

    fix issue when sortBy isnt passed

--- src/decorator.ts
+++ src/decorator.ts
@@ -96,8 +96,8 @@ export const Paginate = createParamDecorator((_data: unknown, ctx: ExecutionCont
     }
 
     const searchBy = parseParam<string>(query.searchBy, singleSplit)
-    const sortBy = parseParam<[string, string]>(query.sortBy, multipleSplit).map(([column, order]) => {
-        const parts = column.split('~');
+    const sortBy = parseParam<[string, string]>(query.sortBy, multipleSplit)?.map(([column, order]) => {
+        const parts = column.split('~')
         return [parts.length > 1 ? parts : column, order] as [string | string[], string]
     })
     const select = parseParam<string>(query.select, multipleAndCommaSplit)
